c82046b02fcb051288f76e50b4febd3923120c49
Reed Lambier <rlambier46@gmail.com>
2016-03-27 15:05:45 -0700

    It will add the first frustrating commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+// number pad variables
+var number1 = parseInt("1");
+  return number1;
+var number2 = parseInt("2");
+  return number1;
+var number3 = parseInt("3");
+  return number1;
+var number4 = parseInt("4");
+  return number1;
+var number5 = parseInt("5");
+  return number1;
+var number6 = parseInt("6");
+  return number1;
+var number7 = parseInt("7");
+  return number1;
+var number8 = parseInt("8");
+  return number1;
+var number9 = parseInt("9");
+  return number1;
+var number0 = parseInt("0");
+  return number1;
+alert("yo");
+});
